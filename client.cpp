fdfc021c9b8bf41474400c56af6bdd9207364e7f
swm_231 <1056338797@qq.com>
2023-03-16 15:40:42 +0800

    bidirectional transmission

--- client.cpp
+++ client.cpp
@@ -6,13 +6,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "util.h"
 
-void errif(bool condition, const char *errmsg){
-    if(condition){
-        perror(errmsg);
-        exit(EXIT_FAILURE);
-    }
-}
+#define BUFFER_SIZE 1024
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -23,30 +19,33 @@ int main() {
     serv_addr.sin_addr.s_addr = inet_addr("8.130.110.118");
     serv_addr.sin_port = htons(8888);
 
-    errif(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket connect erroe");
-    
-    char message[100];
-    // scanf("%s", message);
+    errif(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket connect error");
+
     int cnt=10;
     while(cnt --){
-        bzero(&message, sizeof message);
-        scanf("%s", message);
-        char buf[100];
+        char buf[BUFFER_SIZE];
         bzero(&buf, sizeof buf);
-        ssize_t write_byte = write(sockfd, &message, strlen(message));
+        scanf("%s", buf);
+        ssize_t write_byte = write(sockfd, &buf, strlen(buf));
         if(write_byte == -1){
             printf("socket already disconnect");
             break;
         }
-        /*
+        
         bzero(&buf, sizeof buf);
         ssize_t read_bytes = read(sockfd, buf, sizeof buf);
         buf[read_bytes]='\0';
         if(read_bytes > 0)
             printf("%s\n",buf);
-        if(read_bytes == 0) break;
-        else errif(true, "socket read error");
-        */
+        if(read_bytes == 0){
+            printf("server socket disconnect!\n");
+            break;
+        }
+        else if(read_bytes == -1){
+            close(sockfd);
+            errif(true, "socket read error");
+        }
+        
     }
     close(sockfd);
     return 0;
